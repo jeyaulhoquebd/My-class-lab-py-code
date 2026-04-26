d65e9afcfee4b2aced803277506c8d624249eeb3
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-26 15:59:39 +0600

    git

--- python/main.py
+++ python/main.py
@@ -5,12 +5,13 @@
 #     for n in range(100):
 #         if n % 2 == 0:
 #             continue
-#         sum += n
+#         sum = sum + n
 #         return sum
-#     add = summation()
-#     print("The summation is =", add)
+# add = summation()
+# print("The summation is =", add)
 
 def summation():
+    n = 1
     sum = 0
     for n in range(100):
         if n % 2 == 0:
