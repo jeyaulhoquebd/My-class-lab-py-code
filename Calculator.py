486ed9ea58d434ea0ff86010a6fb93207fe5feb3
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-27 22:33:01 +0600

    py

--- Calculator.py
+++ Calculator.py
@@ -23,3 +23,12 @@ print("4. Divide")
 
 while True:
     #Take input from  the user
+    choice = input("Enter choice(1/2/3/4):")
+
+    #Chek if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number:"))
+            num2 = float(input("Enter second number:"))
+        except ValueError:
+            print("Invalid input. please enter a number.")
